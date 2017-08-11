4572be0af8d37a18d22d863352043732cd7c1558
shin katsumaru <ms.katsumaru@gmail.com>
2017-08-11 17:58:07 +0900

    add 7.1

--- gopl.io/ch7/bytecounter/bytecounter.go
+++ gopl.io/ch7/bytecounter/bytecounter.go
@@ -1,9 +1,30 @@
 package bytecounter
 
+import (
+	"strings"
+)
+
 type ByteCounter int
 
-
-func (c *ByteCounter) Write (p []byte) (int,error ){
+func (c *ByteCounter) Write(p []byte) (int, error) {
 	*c += ByteCounter(len(p))
-	return len(p),nil
+	return len(p), nil
+}
+
+// 7.1
+type WordCounter int
+
+func (c *WordCounter) Count(word string) (int, error) {
+	split := strings.Fields(word)
+	return len(split), nil
+}
+
+type LineCounter int
+
+func (c *LineCounter) Count(word string) (int, error) {
+	if word == "" {
+		return 0, nil
+	}
+	sp := strings.Split(word, "\n")
+	return len(sp), nil
 }
--- gopl.io/ch7/bytecounter/bytecounter_test.go
+++ gopl.io/ch7/bytecounter/bytecounter_test.go
@@ -5,21 +5,66 @@ import (
 )
 
 func TestByteCounter_Write(t *testing.T) {
-	inputs := []struct {
+	candidates := []struct {
 		input    []byte
 		expected int
 	}{
-		{[]byte(""),      0},
-		{[]byte("bar"),   3},
+		{[]byte(""), 0},
+		{[]byte("bar"), 3},
 		{[]byte("hello"), 5},
 		{[]byte("こんにちは"), 15},
 	}
+
 	var c ByteCounter
-	for _, i := range inputs {
-		result,_ := c.Write(i.input)
+	for _, i := range candidates {
+		result, _ := c.Write(i.input)
 
 		if i.expected != result {
 			t.Errorf("expected %v, but %v", i.expected, result)
 		}
 	}
+}
+
+func TestWordCounter_Count(t *testing.T) {
+	candidates := []struct {
+		input    string
+		expected int
+	}{
+		{"", 0},
+		{"foo bar", 2},
+		{"foobar", 1},
+		{"foo  bar", 2},
+		{"foo  bar fuga", 3},
+		{"foo  bar fuga  piyo", 4},
+		{"foo  bar fuga  piyo  ", 4},
+	}
+	var c WordCounter
+	for _, i := range candidates {
+		result, _ := c.Count(i.input)
+
+		if i.expected != result {
+			t.Errorf("expected %v, but %v", i.expected, result)
+		}
+	}
+}
+
+func TestLineCounter_Count(t *testing.T) {
+	candidates := []struct {
+		input    string
+		expected int
+	}{
+		{"foo bar", 1},
+		{"foobar\n", 2},
+		{"", 0},
+	}
+
+	var c LineCounter
+	for _, i := range candidates {
+		result, _ := c.Count(i.input)
+
+		if i.expected != result {
+			t.Errorf("expected %v, but %v", i.expected, result)
+		}
+	}
+
 }
