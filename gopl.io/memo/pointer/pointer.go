c9b3acec41b51f2248558566d5b5f7008aa4a263
shin katsumaru <ms.katsumaru@gmail.com>
2017-08-13 14:34:56 +0900

    Add memo/pointer

---
+++ gopl.io/memo/pointer/pointer.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"reflect"
+	"fmt"
+)
+
+func main() {
+
+	m := map[string]int{}
+	fmt.Println(m)
+	fmt.Println(reflect.TypeOf(m))
+
+	n := make(map[string]int)
+	fmt.Println(n)
+	fmt.Println(reflect.TypeOf(n))
+
+	o := new(map[string]int) // new はポインタをかえす。
+	fmt.Println(o)
+	fmt.Println(reflect.TypeOf(o))
+
+	// 慣習的にポインタ型に対する変数名に決まりとかある？同じ名前だとかなりわかりづらい。
+	// map[]
+	// map[string]int
+	// map[]
+	// map[string]int
+	// &map[]
+	// *map[string]int
+	fmt.Println(&o)
+	fmt.Println(*o)
+	fmt.Println(*o == nil)
+	fmt.Println(o["aaa"])
+
+}
